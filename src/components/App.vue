1ec6a544d0192d40ae84bdb5433926526cf913e8
zede <zede@ventumit.ru>
2018-03-24 16:16:59 +0500

    базовое приложение

---
+++ src/components/App.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+    #app Hello World!
+</template>
+
+<script lang="ts">
+    export default {
+        name: "app"
+    }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
