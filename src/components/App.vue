dc9299f6079f590c7c538e20b00e292d59a5d873
zede <zede@ventumit.ru>
2018-03-27 12:21:39 +0500

    реализован вывод списка песен на экрран

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,9 +1,15 @@
 <template lang="pug">
-    #app {{path}}
+    #app
+        | {{path}}
+        .song(v-for="song in songs")
+            | {{song}}
+            br/
 </template>
 
 <script lang="ts">
     import {Component, Vue} from 'vue-property-decorator'
+    import {aBind} from '../client/atools'
+    import {Song} from "../osuhelper";
 
     @Component
     export default class App extends Vue {
@@ -11,10 +17,20 @@
             return this.$store.state.osuPath;
         }
 
+        get songs() {
+            return this.$store.state.osuSongs.map((e: Song)=> e.name);
+        }
+
         mounted() {
-            if (this.$store.state.osuPath === '') {
-                this.$store.commit('setOsuPath', Vue.osu.path);
-            }
+            aBind(async ()=> {
+                if (this.$store.state.osuPath === '') {
+                    this.$store.commit('setOsuPath', Vue.osu.path);
+                }
+                console.log(this.$store.state.osuSongs);
+                if (this.$store.state.osuSongs.length === 0) {
+                    this.$store.commit('setOsuSongs', await Vue.osu.songs);
+                }
+            });
         }
     }
 </script>
