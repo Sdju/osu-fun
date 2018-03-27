63ffab93868e52aaa5e35605664c00527fa9a6da
zede <zede@ventumit.ru>
2018-03-27 05:00:51 +0500

    переписана инициализация Vue приложения с учетом Vuex

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,7 +1,29 @@
 import Vue from 'vue'
+import {Store, StoreOptions, Plugin, install as VuexPlugin} from 'vuex'
 import App from '../components/App.vue'
 
-Vue.config.productionTip = false;
-new Vue({
-    render: h=>h(App)
-}).$mount('#app');
+import {RootState} from './State'
+import {aBind} from './atools'
+
+aBind(async ()=> {
+
+    Vue.config.productionTip = false;
+    Vue.use(VuexPlugin);
+    const store:Store<RootState> = new Store<RootState>({
+        mutations: {
+            setOsuPath(state: RootState, path: string): RootState {
+                const newState: RootState = state;
+                newState.osuPath = path;
+                return newState;
+            }
+        },
+        state: {
+            osuPath: ''
+        }
+    });
+
+    new Vue({
+        render: h=>h(App),
+        store,
+    }).$mount('#app');
+});
