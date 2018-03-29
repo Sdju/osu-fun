a4d6e1af5bc0dd458913595ca49602b5112e12c7
zede <zede@ventumit.ru>
2018-03-29 12:52:08 +0500

    начата разработка компонента тайтла окна

---
+++ src/components/WindowTitle.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+    .window-title
+        .window-title--icon
+        .window-title--text
+        .window-title--buttons
+            button.window-title--buttons-minimize 🗕
+            button.window-title--buttons-maximmize 🗖
+            button.window-title--buttons-close ×
+</template>
+
+<script lang="ts">
+    export default {
+        name: "window-title"
+    }
+</script>
+
+<style lang="stylus" scoped>
+@import 'styles/palette'
+
+.window-title
+    display flex
+    flex-direction row
+    height 32px
+    width 100%
+    background-color color-main
+</style>
