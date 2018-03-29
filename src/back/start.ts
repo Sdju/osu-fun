7dd92949b07edf990aaeb4cd21999305267cb721
zede <zede@ventumit.ru>
2018-03-30 02:24:12 +0500

    убраны рамки окна

--- src/back/start.ts
+++ src/back/start.ts
@@ -3,7 +3,7 @@ import * as path from 'path'
 import * as url from 'url'
 
 app.on('ready', () => {
-   const win = new BrowserWindow({width: 800, height: 600});
+   const win = new BrowserWindow({width: 800, height: 600, frame: false});
 
    win.loadURL(url.format({
        pathname: path.join(__dirname, '..', 'view', 'index.html'),
