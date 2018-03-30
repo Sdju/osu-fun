977deb8474e715a33592f0f7291728d9e568c19b
zede <zede@ventumit.ru>
2018-03-30 10:20:55 +0500

    компонент WindowTitle обновлен

--- src/components/WindowTitle.vue
+++ src/components/WindowTitle.vue
@@ -1,27 +1,66 @@
 <template lang="pug">
     .window-title
         .window-title--icon
-        .window-title--text
+        .window-title--text {{_title}}
         .window-title--buttons
-            button.window-title--buttons-minimize 🗕
-            button.window-title--buttons-maximmize 🗖
-            button.window-title--buttons-close ×
+            button.window-title--buttons-minimize(v-on:click="minimize") 🗕
+            button.window-title--buttons-maximmize(v-on:click="maximize") {{maximazeSymbol}}
+            button.window-title--buttons-close(v-on:click="close") ×
 </template>
 
 <script lang="ts">
-    export default {
-        name: "window-title"
+    import {Component, Prop, Vue} from 'vue-property-decorator'
+
+    const MaximazeState = {
+        max: '🗖',
+        unmax: '🗗'
+    };
+
+    @Component
+    export default class WindowTitle extends Vue {
+        @Prop()
+        title?: string;
+
+        get _title() {
+            return this.$store.state.window.title;
+        }
+
+        maximazeSymbol: string = MaximazeState.max;
+
+        minimize() {
+            Vue.electron.minimize();
+        }
+
+        maximize() {
+            if (Vue.electron.isMaximized()) {
+                Vue.electron.unmaximize();
+                this.maximazeSymbol = MaximazeState.max;
+            } else {
+                Vue.electron.maximize();
+                this.maximazeSymbol = MaximazeState.unmax;
+            }
+        }
+
+        close() {
+            Vue.electron.close();
+        }
+
+        mounted() {
+            console.log(Vue.electron);
+            this.maximazeSymbol = (Vue.electron.isMaximized())? MaximazeState.unmax : MaximazeState.max;
+        }
     }
 </script>
 
 <style lang="stylus" scoped>
 @import 'styles/palette'
 
+window-title-height = 32px
 .window-title
     display flex
     flex-direction row
     align-items stretch
-    height 32px
+    height window-title-height
     width 100%
     background-color color-main
     -webkit-app-region drag
@@ -29,6 +68,9 @@
 
     &--text
         flex 1 1 auto
+        color white
+        line-height window-title-height
+        padding-left 10px
 
     &--buttons
         display flex
